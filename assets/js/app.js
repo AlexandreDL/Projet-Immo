4d1ac1e7729157a1ff9219bd9a66d2c4506a38a9
AlexandreDL <alexandre.deluca@free.fr>
2019-08-30 17:13:35 +0200

    feature: delete img in form

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,49 +1,40 @@
-/*
- * Welcome to your app's main JavaScript file!
- *
- * We recommend including the built version of this JavaScript file
- * (and its CSS file) in your base layout (base.html.twig).
- */
-
-// any CSS you require will output into a single css file (app.css in this case)
 let $ = require('jquery')
-
 require('../css/app.css');
 require('select2')
 
 $('select').select2()
 let $contactButton = $('#contactButton')
-$('#contactButton').click(e => {
-    e.preventDefault()
-    $('#contactForm').slideDown();
-    $contactForm.slideUp();
-} )
-
-
-//Suppression des éléments
-document.querySelectorAll('[data-delete]').forEach(a => {
-    a.addEventListener('click', e => {
-        e.preventDefault()
-        fetch(a.getAttribute('href'), {
-            method:'DELETE',
-            headers: {
-                'X-Requested-with': 'XMLHttpRequest',
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({'_token': a.CDATA_SECTION_NODE.token})
-        }).then(response => response.json())
-        .then(data => {
-            if (data.success) {
-                a.parentNode.parentNode.removeChild(a.parentNode)
-            } else {
-                alert(data.error)
-            }
-        })
-        .catch(e => alert(e))
-    })
+$contactButton.click(e => {
+  e.preventDefault();
+  $('#contactForm').slideDown();
+  $contactButton.slideUp();
 })
 
+// Suppression des éléments
+document.querySelectorAll('[data-delete]').forEach(a => {
+  a.addEventListener('click', e => {
+    e.preventDefault()
+    fetch(a.getAttribute('href'), {
+      method: 'DELETE',
+      headers: {
+        'X-Requested-With': 'XMLHttpRequest',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({'_token': a.dataset.token})
+    }).then(response => response.json())
+      .then(data => {
+        if (data.success) {
+          a.parentNode.parentNode.removeChild(a.parentNode)
+        } else {
+          alert(data.error)
+        }
+      })
+      .catch(e => alert(e))
+  })
+})
+
+
 // Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
-// const $ = require('jquery');
+// var $ = require('jquery');
 
 console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
