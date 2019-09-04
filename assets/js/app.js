98e9361a901fa79b9d70ab9a37b986c35fb6a880
AlexandreDL <alexandre.deluca@free.fr>
2019-09-04 13:31:43 +0200

    Save before Carousel try

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,8 @@
 import Places from 'places.js'
 import Map from './modules/map.js'
-import "slick-carousel";
-import "slick-carousel/slick/slick-theme.css";
-import "slick-carousel/slick/slick.css";
+// import "slick-carousel";
+// import "slick-carousel/slick/slick-theme.css";
+// import "slick-carousel/slick/slick.css";
 
 Map.init()
 
@@ -35,10 +35,10 @@ let $ = require('jquery')
 require('../css/app.css');
 require('select2')
 
-$('.data-slider').slick({
-  dots: true,
-  arrows: true
-})
+// $('.data-slider').slick({
+//   dots: true,
+//   arrows: true
+// })
 $('select').select2()
 let $contactButton = $('#contactButton')
 $contactButton.click(e => {
